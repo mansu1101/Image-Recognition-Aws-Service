59a3a676df1362b0f60b11f7d8d2d3a885e29539
akshay rajpal <akshay.rajpal@intimetec.com>
2020-07-02 18:52:50 +0530

    added registration and attendance

--- server/employeeService.js
+++ server/employeeService.js
@@ -1,4 +1,6 @@
 const connectionManager = require("./connectionManager");
+let awsService = require("./awsService")
+
 exports.getEmployeeList = function (next) {
     let db = connectionManager.instance();
     db.collection("employee").find({}).toArray(function (err, res) {
@@ -21,4 +23,49 @@ exports.registerEmployee = function (empDetails, next) {
             next(null, res.result);
         }
     });
+}
+
+exports.registerEmployeeImage = function (empData, next) {
+    awsService.registerFace(empData.image, (err, data) => {
+        if (!err) {
+            console.log("S3 upload successful - ", data); // successful response
+            let db = connectionManager.instance();
+            let query = {
+                empId: parseInt(empData.data.empId)
+            };
+            db.collection("employee").update(query,
+                {$set: {faceId: data.key}}, function (error, result) {
+                    next(error, result);
+                })
+        } else {
+            console.error("S3 upload error - ", err); // successful response
+        }
+    });
+}
+
+exports.submitAttendance = function (file, next) {
+    awsService.matchFaces(file, function (err, data) {
+        if (!err) {
+            console.log("face matched successfully")
+            let db = connectionManager.instance();
+            db.collection("employee").find({faceId: data.faceId}).toArray(function (error, result) {
+                if (result && result.length && result[0].empId) {
+                    let date = new Date();
+                    let attendanceData = {
+                        empId: result[0].empId,
+                        status: 1,
+                        date: date,
+                    }
+                    db.collection("attendance").insertOne(attendanceData, function (err, data) {
+                        console.log("attendance submitted successfully");
+                        next(null, data);
+                    })
+                } else {
+                    next("No employee with faceId", null)
+                }
+            })
+        } else {
+            next("your face does not matched to any face", null);
+        }
+    })
 }
