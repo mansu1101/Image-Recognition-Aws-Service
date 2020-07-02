c7e7a35422dec5489e0d0daa6a8c312032a8c7db
Manish Joshi <mansujoshi89@gmail.com>
2020-07-02 11:58:18 +0530

    Develop (#9)
    * Updating package.json file
    
    * updating cdn path for dependencies
    
    * Updating index file dependecies
    
    * Updating client code
    
    * Added connection manger for employee service
    
    Co-authored-by: Manish <manish.joshi@intimetec.com>

---
+++ server/connectionManager.js
@@ -0,0 +1,31 @@
+const config = require("./config");
+let MongoClient = require('mongodb').MongoClient,
+    instance = undefined;
+
+function getConnection(next) {
+    if (instance) {
+        return next(null, instance);
+    } else {
+        MongoClient.connect(config.mongoUrl, function (err, db) {
+            if (err) {
+                throw err;
+                next(err, null);
+            } else {
+                console.log("Database created!");
+                instance = db;
+                next(null, instance);
+            }
+        });
+    }
+}
+getConnection((err, db) => {
+    if(err){
+        console.log("Error while creating the database:", err);
+    }else{
+        return db;
+    }
+});
+exports.instance = function () {
+    console.log("returning instance");
+   return instance;
+}
