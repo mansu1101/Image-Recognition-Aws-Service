98886a8236b60d33ec107941000114483a0bf58e
Manish Joshi <mansujoshi89@gmail.com>
2020-07-02 12:42:31 +0530

    Develop (#10)
    * Updating package.json file
    
    * updating cdn path for dependencies
    
    * Updating index file dependecies
    
    * Updating client code
    
    * Added connection manger for employee service
    
    * created routs for register employee and get list
    
    Co-authored-by: Manish <manish.joshi@intimetec.com>

--- server/router.js
+++ server/router.js
@@ -1,16 +1,37 @@
 // Import dependencies.
 var multer = require("multer")();
 var express = require("express");
+var bodyParser = require('body-parser');
 var path = require("path");
 var awsService = require("./awsService");
+const employeeService = require("./employeeService");
 
 module.exports = function (app) {
   // Load the UI files.
   app.use(express.static(path.resolve(__dirname + "/../"  + "/client")));
+  app.use(bodyParser.json());
 
   // Register /api/recognize endpoint for HTTP POST method.
   app.post("/api/recognize", multer.single("image"), (req, res) => {
     awsService.matchFaces(req.file, (error, response) => {
+      if (error) {
+        res.status(500).json({error: error});
+      } else {
+        res.status(200).json(response);
+      }
+    });
+  });
+  app.get("/api/empList", (req, res) => {
+    employeeService.getEmployeeList((error, response) => {
+      if (error) {
+        res.status(500).json({error: error});
+      } else {
+        res.status(200).json(response);
+      }
+    });
+  });
+  app.post("/api/register", (req, res) => {
+    employeeService.registerEmployee(req.body, (error, response) => {
       if (error) {
         res.status(500).json({error: error});
       } else {
